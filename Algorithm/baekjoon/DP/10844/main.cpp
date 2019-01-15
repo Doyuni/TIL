28b60a1ac5fc885ec96d1d485bf526c6dd8a5471
Doyuni <gdypc@naver.com>
2019-01-15 22:46:46 +0900

    add a c++ code

---
+++ Algorithm/baekjoon/DP/10844/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+long long d[101][10];
+int main()
+{
+	int n;
+	const long long mod = 1000000000LL;
+	scanf(" %d", &n);
+	for (int i = 1; i <= 9; i++)
+	{
+		d[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			if (j > 0) d[i][j] += d[i - 1][j - 1]; // 0일 때는 1만 가능
+			if (j < 9) d[i][j] += d[i - 1][j + 1]; // 9일 때는 8만 가능
+			//이를 하지 않으면 엄청나게 큰 수가 나와 값을 제대로 다루지 못 한다.
+			d[i][j] %= mod; // (A+B)%C = ((A%C)+(B%C))%C
+		}
+	}
+	long long result = 0;
+	for (int i = 0; i <= 9; i++)
+	{
+		result += d[n][i];
+	}
+	printf("%lld\n", result % mod);
+	return 0;
+}
